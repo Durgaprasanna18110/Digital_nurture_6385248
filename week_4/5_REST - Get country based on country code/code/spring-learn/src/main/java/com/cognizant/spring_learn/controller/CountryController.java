a6ca937a98ac268449a9074a9030c07955ad402e
G Durgaprasanna <gudladurgaprasanna1@gmail.com>
2025-07-12 15:28:11 +0530

    Add files via upload

---
+++ week_4/5_REST - Get country based on country code/code/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -0,0 +1,24 @@
+package com.cognizant.spring_learn.controller;
+
+import com.cognizant.spring_learn.Country;
+import com.cognizant.spring_learn.service.CountryService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List; // ✅ MISSING IMPORT ADDED
+
+@RestController
+public class CountryController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CountryController.class);
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/countries")
+    public List<Country> getAllCountries() {
+        return countryService.getAllCountries();
+    }
+}
